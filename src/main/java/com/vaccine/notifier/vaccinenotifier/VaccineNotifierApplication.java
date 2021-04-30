3364416913f74a7103f9d41c983a2c5ac4ac17ba
khush <khushgandhisince1998@gmail.com>
2021-05-01 01:33:42 +0530

    intial commit

---
+++ src/main/java/com/vaccine/notifier/vaccinenotifier/VaccineNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.vaccine.notifier.vaccinenotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineNotifierApplication.class, args);
+	}
+
+}
